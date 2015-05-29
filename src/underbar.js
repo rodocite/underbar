ac034b07df5d74209b5105da5171208e748099f5
Rodolfo Yabut <rodolfoyabut@gmail.com>
2015-05-29 08:34:49 -0700

    Use filter to pass uniq.

--- src/underbar.js
+++ src/underbar.js
@@ -88,7 +88,7 @@
         result.push(val);
       }
     });
-    
+
     return result;
   };
 
@@ -104,10 +104,10 @@
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
     var result = [];
-    _.each(array, function(val) {
+    _.filter(array, function(val) {
       if(_.indexOf(result, val) === -1) {
         result.push(val);
-      }
+      }      
     });
     return result;
   };
